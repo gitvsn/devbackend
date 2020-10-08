5a52ba2b0d5d659f9e1b07f86873cb606ea96770
Andrei Hais <andrei.gais@i.ua>
2020-10-08 18:36:42 +0300

    add @EnableWebSecurity annotation to SecurityConfig

--- src/main/java/com/vsn/config/SecurityConfig.java
+++ src/main/java/com/vsn/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -21,6 +22,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
  */
 @EnableScheduling
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtTokenProvider jwtTokenProvider;
