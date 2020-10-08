c160da7eb3fe74819209fc0848b235480db09745
Andrei Hais <andrei.gais@i.ua>
2020-10-08 17:47:04 +0300

    add column name to user token

--- src/main/java/com/vsn/entities/registration/UserToken.java
+++ src/main/java/com/vsn/entities/registration/UserToken.java
@@ -3,14 +3,18 @@ package com.vsn.entities.registration;
 import com.vsn.entities.BaseEntity;
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 
 @Data
 @Entity
-@Table(name = "token")
+@Table(name = "user_token")
 public class UserToken extends BaseEntity {
+    @Column(name = "user_id")
     private long userId;
+
+    @Column(name = "user_token")
     private String token;
 }
